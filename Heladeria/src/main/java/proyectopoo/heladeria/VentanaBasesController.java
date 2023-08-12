3e4a7da80dd7b32ea627df4b68f16fed7bd62683
Bolaños Fajardo Tomás Steven <tbolanos@espol.edu.ec>
2023-08-12 16:15:17 -0500

    grafico vectorial añadido por que no encontre figuritas bonitas

--- Heladeria/src/main/java/proyectopoo/heladeria/VentanaBasesController.java
+++ Heladeria/src/main/java/proyectopoo/heladeria/VentanaBasesController.java
@@ -4,16 +4,18 @@
  */
 package proyectopoo.heladeria;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * FXML Controller class
@@ -26,8 +28,6 @@ public class VentanaBasesController implements Initializable {
     private VBox nodoVbox;
     @FXML
     private HBox nodoHbox31;
-    @FXML
-    private ImageView nodoImageView31;
     @FXML
     private HBox nodoHbox32;
     @FXML
@@ -54,15 +54,47 @@ public class VentanaBasesController implements Initializable {
     private Label nodoLabelprecio2;
     @FXML
     private Label nodoLabelprecio3;
+    @FXML
+    private Canvas nodoCanvas;
 
     /**
      * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+     *//*"heladoUpper2.png""heladoUpper1.png"*/
 
+    
+@Override
+public void initialize(URL url, ResourceBundle rb) {
+
+        
+        GraphicsContext gc = nodoCanvas.getGraphicsContext2D();
+ 
+
+        dibujarHeladoDerretido(gc);
+}
+
+     private void dibujarHeladoDerretido(GraphicsContext gc) {
+        double centroX = 411;
+        double inicioY = 0; // Mantenemos el signo negativo
+        double escalaX = 100;
+        double escalaY = 80;
+
+        gc.setFill(Color.hsb(180, 1, 1, 0.5));
+
+        gc.beginPath();
+
+        for (double x = -Math.PI * 2; x <= Math.PI * 2; x += 0.01) {
+            double y = -Math.abs(Math.sin(3 * x)) * escalaY; // Cambiamos el signo a negativo
+            double xPixel = centroX + x * escalaX;
+            double yPixel = inicioY - y; // Mantenemos el signo negativo
+
+            gc.lineTo(xPixel, yPixel);
+        }
+
+        gc.closePath();
+        gc.fill();
+    }
+
+    
     @FXML
     private void setBase1(ActionEvent event) {
     }
